9b1f56cadd3ee86bc0fabaca21ff6e5a91c31483
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2020-05-18 10:06:35 -0400

    Campus UPR: rename baseUrl to www.upr.edu.cu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  baseUrl: 'http://campus.upr.edu.cu',
-  productionAppUrl: 'http://campus.upr.edu.cu/api',
+  baseUrl: 'http://www.upr.edu.cu',
+  productionAppUrl: 'http://www.upr.edu.cu/api',
   newsUrl: 'http://news.upr.edu.cu/wp-json/wp/v2',
   projectURL: 'http://gespro.upr.edu.cu:5000/graphql',
   syncURL: 'http://sync.upr.edu.cu/api',
